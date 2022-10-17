04a2119ee7f7a0a845905cbdec7433fffc68b754
Aryaman Godara <godara.aryaman@gmail.com>
2022-10-17 17:09:42 +0530

    initial skelton

---
+++ src/containers/BurgerBuilder.js
@@ -0,0 +1,12 @@
+import React,{ Component } from "react";
+
+class BurgerBuilder extends Component{
+    render (){
+        return <>
+                <div>Burger</div>
+                <div>Burger Controls</div>
+            </>;
+    }
+}
+
+export default BurgerBuilder;
